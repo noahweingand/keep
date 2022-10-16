7ca2b562bec306b60b9baf4de5be26577493c06b
Noah Weingand <noahweingand@gmail.com>
2022-10-16 01:27:54 -0400

    change width/height/viewport

--- components/svgs/KeepGuySVG.tsx
+++ components/svgs/KeepGuySVG.tsx
@@ -3,14 +3,14 @@ export const KeepGuySvg = () => {
     <svg
       version="1.0"
       xmlns="http://www.w3.org/2000/svg"
-      width="390.000000pt"
-      height="622.000000pt"
-      viewBox="0 0 402.000000 622.000000"
+      width="402.000000pt"
+      height="600.000000pt"
+      viewBox="0 0 402.000000 500.000000"
       preserveAspectRatio="xMidYMid meet"
-      className="blackMirror"
+      className="flex blackMirror"
     >
       <g
-        transform="translate(0.000000,622.000000) scale(0.100000,-0.100000)"
+        transform="translate(0.000000,600.000000) scale(0.100000,-0.100000)"
         fill="#ffffff"
         stroke="none"
       >
