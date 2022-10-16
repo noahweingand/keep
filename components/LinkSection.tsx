0b87c421706ae58cd40a8e4410161776dda9d7b9
Noah Weingand <noahweingand@gmail.com>
2022-10-16 01:28:33 -0400

    fit links for smaller screens

--- components/LinkSection.tsx
+++ components/LinkSection.tsx
@@ -3,8 +3,8 @@ import { NavLink } from './Link';
 
 export const LinkSection = () => {
   return (
-    <div className="tui-bg-blue-black flex w-full">
-      <div className="flex flex-row w-full justify-center gap-4 p-4">
+    <div className="tui-bg-blue-black w-full">
+      <div className="flex flex-wrap w-full justify-center gap-4 p-4">
         {links.map((props) => {
           return <NavLink key={props.id} {...props} />;
         })}
