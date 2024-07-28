3791529ed575ea4971948ca5e4d7f13956a41989
Noah Weingand <43417465+noahweingand@users.noreply.github.com>
2024-07-28 11:17:54 -0400

    chore: add EU tour dates

--- constants/tour-dates.ts
+++ constants/tour-dates.ts
@@ -1,20 +1,104 @@
 export const tourDates = [
   {
-    id: 111,
+    id: 1,
     venue: 'The Camel',
     date: '2022-12-15',
     location: 'Richmond, VA',
   },
   {
-    id: 222,
+    id: 2,
     venue: 'TV EYE',
     date: '2022-12-16',
     location: 'Ridgewood, NY',
   },
   {
-    id: 333,
+    id: 3,
     venue: 'The NorVa',
     date: '2022-12-17',
     location: 'Norfolk, VA',
   },
+  {
+    id: 4,
+    venue: 'Loose Cobra',
+    date: '2024-08-17',
+    location: 'Tolono, IL'
+  },
+  {
+    id: 5,
+    venue: 'Mobious Records',
+    date: '2024-08-24',
+    location: 'Washington, D.C.'
+  },
+  {
+    id: 6,
+    venue: 'Burgerhaus',
+    date: '2024-10-18',
+    location: 'Bremen (DE)'
+  },
+  {
+    id: 7,
+    venue: 'La Zone',
+    date: '2024-10-19',
+    location: 'Liège (BE)'
+  },
+  {
+    id: 8,
+    venue: 'villaWuller',
+    date: '2024-10-20',
+    location: 'Trier (DE)'
+  },
+  {
+    id: 9,
+    venue: 'La Source Beer Co.',
+    date: '2024-10-22',
+    location: 'Brussels (BE)'
+  },
+  {
+    id: 10,
+    venue: 'Supersonic',
+    date: '2024-10-23',
+    location: 'Paris (FR)'
+  },
+  {
+    id: 11,
+    venue: 'TBA',
+    date: '2024-10-24',
+    location: 'Brussels (BE)'
+  },
+  {
+    id: 12,
+    venue: 'OCCII',
+    date: '2024-10-25',
+    location: 'Amsterdam (NL)'
+  },
+  {
+    id: 13,
+    venue: 'Privat',
+    date: '2024-10-26',
+    location: 'Cologne (BE)'
+  },
+  {
+    id: 14,
+    venue: 'Juz',
+    date: '2024-10-27',
+    location: 'Mannheim (DE)'
+  },
+  {
+    id: 15,
+    venue: 'Blechschloss',
+    date: '2024-10-29',
+    location: 'Dresden (DE)'
+  },
+  {
+    id: 16,
+    venue: 'Schokoladen',
+    date: '2024-10-30',
+    location: 'Berlin (DE)'
+  },
+  {
+    id: 17,
+    venue: 'The Canal Club',
+    date: '2024-11-02',
+    location: 'Richmond, VA'
+  }
 ];
