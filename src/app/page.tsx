81d893580efd60df10ad3602e61da3896f531bbc
Noah Weingand <noahweingand@gmail.com>
2025-03-18 19:37:48 -0400

    chore: responsive carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <Item />
           <Item />
         </CarouselContent>
-        <div className="flex justify-center mt-8 gap-4">
+        <div className="flex justify-center gap-6 sm:gap-4">
           <CarouselPrevious className="static text-white bg-blue-950/90" />
           <CarouselNext className="static text-white bg-blue-950/90" />
         </div>
@@ -30,8 +30,8 @@ export default function Home() {
 function Item() {
   return (
     <CarouselItem className="basis-full pl-6">
-      <div className="flex flex-col sm:flex-row justify-center items-center py-20">
-        <div className="relative mr-0 sm:mr-12">
+      <div className="flex flex-col sm:flex-row justify-center items-center pt-20 pb-12">
+        <div className="relative mr-3 ml-3 sm:mr-12 sm:ml-6 xl:ml-0">
           <div
             className="absolute animate-pulse rounded-full bg-gradient-to-t from-[#4E4A46] via-[#97805A] to-[#E8783A] blur-2xl opacity-10"
             style={{
@@ -58,62 +58,3 @@ function Item() {
     </CarouselItem>
   );
 }
-
-// export default function Home() {
-//   return (
-//     <main className="w-full h-screen m-auto bg-blue-950/90" style={{ width: "100vw" }}>
-//       <Carousel>
-//         <CarouselContent className="min-h-1/2">
-//           <Item />
-//           <Item />
-//           <Item />
-//           {/* <CarouselItem style={{ clipPath: "inset(0px 0px 0px 0px)" }}>
-//               <div className="flex justify-center items-center">Hello</div>
-//             </CarouselItem>
-//             <CarouselItem style={{ clipPath: "inset(0px 0px 0px 0px)" }}>
-//               <div className="flex justify-center items-center">World</div>
-//             </CarouselItem>
-//             <CarouselItem style={{ clipPath: "inset(0px 0px 0px 0px)" }}>
-//               <div className="flex justify-center items-center">Done</div>
-//             </CarouselItem> */}
-//         </CarouselContent>
-//         <div className="flex justify-center mt-4 gap-2">
-//           <CarouselPrevious className="static text-white bg-blue-950/90" />
-//           <CarouselNext className="static text-white bg-blue-950/90" />
-//         </div>
-//       </Carousel>
-//     </main>
-//   );
-// }
-
-// export default function Home() {
-//   return (
-//     <main className="w-full h-screen m-auto bg-blue-950/90" style={{ width: "100vw" }}>
-//       <div className="flex flex-col sm:flex-row min-h-screen space-y-6 sm:space-y-0 justify-center items-center mx-3 sm:mx-8">
-//         <div className="relative mr-0 sm:mr-12">
-//           <div
-//             className="absolute animate-pulse rounded-full bg-gradient-to-l from-[#4E4A46] via-[#97805A] to-[#E8783A] blur-2xl opacity-10"
-//             style={{
-//               inset: "clamp(-2rem, -15%, -5rem)",
-//             }}
-//           ></div>
-//           <Image
-//             className="relative rounded-lg shadow-lg"
-//             src="/images/albums/almost_static.jpeg"
-//             alt="Album artwork for Almost Static by Keep"
-//             width={500}
-//             height={500}
-//           />
-//         </div>
-//         <div className="flex flex-col h-full justify-center text-white">
-//           <h2 className="text-4xl font-semibold mb-3">Almost Static</h2>
-//           <h3 className="text-xl">Our newest single {`"Single Name"`} is out now. </h3>
-//           <div className="flex flex-row space-x-3 items-center mt-4">
-//             <Button>Pre-order</Button>
-//             <Button>Listen</Button>
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
